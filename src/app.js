406f82b982d2d66e630986f29e9394440e2deaa8
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-23 22:43:37 -0700

    Still can't get console log to work. Not reading app.js or quizScore.js Created a basic test.js folder to see if it would appear in console and it did not

--- src/app.js
+++ src/app.js
@@ -6,27 +6,38 @@ const schoolInput = document.getElementById('school-answer');
 const pnwInput = document.getElementById('pnw-answer');
 const animalInput = document.getElementById('animal-answer');
 
+const resultParagraph = document.getElementById('result-paragraph')
+
 submitButton.addEventListener('click', () => {
+    event.preventDefault();
+
    const schoolAnswer= schoolInput.value;
    const pnwAnswer = pnwInput.value;
    const animalAnswer = animalInput.value;
 
-   let score = 0;
+   const score = quizScore(schoolAnswer, pnwAnswer, animalAnswer);
 
-   let scoreEl = document.getElementById('score');
+   const message = 'Your got ' + score + 'correct! That is ' + (score / 3) * 100 + '%';
+   resultParagraph.textContent = message;
 
-   if(schoolAnswer.trim () === '' || pnwAnswer.trim () === '' || animalAnswer.trim () === '') {
-        scoreEl.textContent = 'Please fill out all options.'; }
-        else {
-            score = quizScore(schoolAnswer, pnwAnswer, animalAnswer);
-            if(score <= 2){
-                scoreEl.classList.add('bad');
-                scoreEl.classList.remove('good');
-            } else {
-                scoreEl.classList.remove('bad');
-                scoreEl.classList.add('add');
-            }
 
-            scoreEl.textContent = 'Your got ' + score + 'correct! That is ' + (score / 3) * 100 + '%';
-        }
+//   TRYING TO MAKE RESULTS DIFFERENT COLORS, BUT CAN'T SEE RESULTS 
+// let score = 0;
+
+//    let scoreEl = document.getElementById('score');
+
+//    if(schoolAnswer.trim () === '' || pnwAnswer.trim () === '' || animalAnswer.trim () === '') {
+//         scoreEl.textContent = 'Please fill out all options.'; }
+//         else {
+//             score = quizScore(schoolAnswer, pnwAnswer, animalAnswer);
+//             if(score <= 2){
+//                 scoreEl.classList.add('bad');
+//                 scoreEl.classList.remove('good');
+//             } else {
+//                 scoreEl.classList.remove('bad');
+//                 scoreEl.classList.add('add');
+//             }
+
+//             scoreEl.textContent = 'Your got ' + score + 'correct! That is ' + (score / 3) * 100 + '%';
+//         }
    });
--- src/quizScore.js
+++ src/quizScore.js
@@ -1,21 +1,22 @@
 function quizScore(schoolAnswer, pnwAnswer, animalAnswer){
     schoolAnswer = schoolAnswer.toLowerCase();
     animalAnswer = animalAnswer.toLowerCase();
-    let score = 0
+
+    let score = 0;
 
     if(schoolAnswer === 'glass' || schoolAnswer === 'glass construction') {
-        score += 1;
+        score +=1;
     }
 
     if(pnwAnswer === 'pittsburgh' || pnwAnswer === 'pennsylvania' || pnwAnswer === 'pittsburgh, pennsylvania'){
-        score += 1;
+        score +=1;
     }
     
     if(animalAnswer === 'raccoon'){
-        score += 1;
+        score +=1;
     }
 
     return score;
-};
+}
 
 export default quizScore;
