dc918fd028c7d47f66d19d048ed71b9306fc4d3c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-23 19:59:59 -0700

    made variables for console log, but they're not showing up after several attempts to fix thing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const school = document.getElementById('school-answer');
 const pnw = document.getElementById('pnw-answer');
 const animal = document.getElementById('animal-answer');
+
+const button = document.getElementById('submit-button');
+const resultParagraph = document.getElementById('result-paragraph');
+
+console.log(button);
+button.addEventListener('click', (event) => {
+   event.preventDefault();
+
+   const schoolValue = school.value;
+   const pnwValue = pnw.value;
+   const animalValue = animal.value;
+
+   console.log(schoolValue, 'school-answer');
+   
+   // is not showing up in console log. don't know issue. had several eyes look at it and still a wtf
+    
+});
